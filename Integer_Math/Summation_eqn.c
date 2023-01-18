2a3e08bcc3e388e48992cee49976d68bf31041a8
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-01-18 22:28:26 +0530

    Bugs & Fixes (#12)

--- Integer_Math/Summation_eqn.c
+++ Integer_Math/Summation_eqn.c
@@ -1,9 +1,13 @@
-/*100 sum x=0 (15x + 30 sin(10xy) + cuberoot(xy) + y + 1)
+/*
+    Author: Advait Thale
+
+    Summation of eqn: 100 sum x=0 (15x + 30 sin(10xy) + cuberoot(xy) + y + 1)
 
  */
 
 #include <stdio.h>
 #include <math.h>
+#define MPI 3.14159265358979323846
 
 int x;
 const int y = 63;
@@ -16,6 +20,8 @@ int main()
         sum = sum + (15 * x + 30 * sin(10 * x * y) + cbrt(x * y) + y + 1);
         // sum = sum + (x + 63);
     }
-    printf("%f", sum);
+    printf("%0.14f", sum);
+    printf("\nDone!!\n");
+    printf("%0.20f", MPI);
     return 0;
 }
